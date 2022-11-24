16c0f74eada1803a7b3187f880bb52f652602b50
NNAMANI GOODNESS <goodybag0336@gmail.com>
2022-11-24 01:28:44 -1200

    added css and js file location to html

---
+++ script.js
@@ -0,0 +1,6 @@
+let hometown =  prompt("What is your hometown?");
+alert(hometown);
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let fullName = firstName + " " + lastName;
+salert(fullName);
